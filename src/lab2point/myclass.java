8ba0ad01da40ceb23b2a4892df9701a57c04c409
Track-suite <youngunner1998@gmail.com>
2021-02-16 20:05:44 +0530

    this time addin 2 classes

---
+++ src/lab2point/myclass.java
@@ -0,0 +1,10 @@
+package lab2point;
+
+public class myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/labsheet/homeLand.java
@@ -0,0 +1,11 @@
+package labsheet;
+
+public class homeLand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 System.out.println("LAst time");
+	}
+
+}
